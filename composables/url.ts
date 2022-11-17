3e4d9d4b6a8ac3b9d53991e4dd202df104e54a39
inannan423 <605031236@qq.com>
2022-11-17 09:49:45 +0800

    Update url.ts

--- composables/url.ts
+++ composables/url.ts
@@ -1,2 +1,3 @@
 export const useUrl = () =>
-  process.dev ? "http://localhost:3000" : "https://jetpanodoc.vercel.app";
+  // process.dev ? "http://localhost:3000" : "https://jetpanodoc.vercel.app";
+  "https://jetpanodoc.vercel.app";
